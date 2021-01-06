255fbb8c136e3bd279ef747e46d612144a6f260c
yh <yh@naver.com>
2021-01-06 17:37:45 +0900

    two

--- src/main/java/kh/spring/controller/YHController.java
+++ src/main/java/kh/spring/controller/YHController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class YHController {
 	
 	@RequestMapping("/")
-	public String home() {
+	public String YH() {
 		return "home";
 	}
 }
