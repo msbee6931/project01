dae0071ac081a2df7e1182ab2b1b09bcb2c59bc7
ph <ph@ph.com>
2021-01-07 11:08:41 +0900

    PHController recreated

--- src/main/java/kh/spring/controller/PHController.java
+++ src/main/java/kh/spring/controller/PHController.java
@@ -1,11 +1,14 @@
 package kh.spring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PHController {
-	
-	public void ph() {
-		System.out.println("변경 확인 좀 부탁드려요");
+
+	@RequestMapping("request.ph")
+	public String request() {
+		System.out.println("병합 확인 부탁 드려요!!");
+		return "ph";
 	}
 }
